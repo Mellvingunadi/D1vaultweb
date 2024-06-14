28e24ad8b1c6ae2c2d0ffa5c7b6d19689fe5b2f5
Mellvin <mellvingunadi@gmail.com>
2024-06-14 14:40:23 +0700

    firstCommit

---
+++ src/components/Collection.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import config from '../config/index.json';
+
+const Collection = () => {
+  const { collection } = config;
+  const { title, items } = collection;
+
+  return (
+    <section className={`bg-[#232323] py-8`} id="collection">
+      <div className={`container mx-auto px-2 pt-4 pb-12 text-primary`}>
+        <h1
+          className={`w-full my-2 text-5xl font-bold leading-tight text-center text-white`}
+        >
+          {title}
+        </h1>
+        <div className={`flex flex-wrap justify-center pt-12 my-12 sm:my-4`}>
+          {items.map((image) => (
+            <div
+              key={image.img}
+              className={`w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4`}
+            >
+              <img
+                src={image.img}
+                alt={image.img}
+                className={`w-full h-full object-cover rounded-lg shadow-lg`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Collection;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,21 +7,11 @@ import { Link } from 'react-scroll';
 import config from '../config/index.json';
 
 const Menu = () => {
-  const { navigation, company, callToAction } = config;
+  const { navigation, company } = config;
   const { name: companyName, logo } = company;
 
   return (
     <>
-      <svg
-        className={`hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-background transform translate-x-1/2`}
-        fill="currentColor"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        aria-hidden="true"
-      >
-        <polygon points="50,0 100,0 50,100 0,100" />
-      </svg>
-
       <Popover>
         <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
           <nav
@@ -32,11 +22,15 @@ const Menu = () => {
               <div className="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span className="sr-only">{companyName}</span>
-                  <img alt="logo" className="h-16 w-auto sm:h-16" src={logo} />
+                  <img
+                    alt="logo"
+                    className="h-[80px] w-auto sm:h-[45px]"
+                    src={logo}
+                  />
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
                   <Popover.Button
-                    className={`bg-background rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary`}
+                    className={`rounded-md p-2 inline-flex items-center justify-center text-white hover:text-primary hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary`}
                   >
                     <span className="sr-only">Open main menu</span>
                     <MenuIcon className="h-6 w-6" aria-hidden="true" />
@@ -53,17 +47,11 @@ const Menu = () => {
                   duration={1000}
                   key={item.name}
                   to={item.href}
-                  className="font-medium text-gray-500 hover:text-gray-900"
+                  className="font-medium text-white hover:text-[#FE0201] cursor-pointer"
                 >
                   {item.name}
                 </Link>
               ))}
-              <a
-                href="#"
-                className={`font-medium text-primary hover:text-secondary`}
-              >
-                Call to action
-              </a>
             </div>
           </nav>
         </div>
@@ -82,11 +70,11 @@ const Menu = () => {
             className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
           >
             <div
-              className={`rounded-lg shadow-md bg-background ring-1 ring-black ring-opacity-5 overflow-hidden`}
+              className={`rounded-lg shadow-md bg-gray-800 ring-1 ring-black ring-opacity-5 overflow-hidden`}
             >
               <div className="px-5 pt-4 flex items-center justify-between">
                 <div>
-                  <img className="h-8 w-auto" src={logo} alt="" />
+                  <img className="h-[60px] w-auto" src={logo} alt="" />
                 </div>
                 <div className="-mr-2">
                   <Popover.Button
@@ -106,18 +94,12 @@ const Menu = () => {
                     duration={1000}
                     key={item.name}
                     to={item.href}
-                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
                   </Link>
                 ))}
               </div>
-              <a
-                href={callToAction.href}
-                className={`block w-full px-5 py-3 text-center font-medium text-primary bg-gray-50 hover:bg-gray-100`}
-              >
-                {callToAction.text}
-              </a>
             </div>
           </Popover.Panel>
         </Transition>
